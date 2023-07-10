feb285dd950b7d74558e364e8d6f98a97ee71b61
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-07-10 22:14:03 +0530

    Add. Signup request body struct

---
+++ requests/signupRequest.go
@@ -0,0 +1,9 @@
+package requests
+
+type SignupRequest struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Username  string `json:"username" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+}
